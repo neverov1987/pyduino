28c9a1610e79ec4481f6ea80d6f18d9238638c8a
Неверов Михаил Александрович <mikhail.neverov@gost-group.com>
2020-10-12 20:06:11 +0300

    add commets to arduino src

--- enable_led_over_com.ino
+++ enable_led_over_com.ino
@@ -1,50 +1,52 @@
-
+// Определение стартовых значений
 int datafromUser=0;
 int status_1='0';
 int status_2='0';
 int status_3='0';
 int status_4='0';
-char status_list[4];
+char status_list[4]; // назанчение массива для передачи информации по запросу статуса
 void setup() {
   // put your setup code here, to run once:
-  pinMode( LED_BUILTIN , OUTPUT );
-  Serial.begin(9600);
+  pinMode( LED_BUILTIN , OUTPUT ); //Назанчение пина светодиода (внутренний светодиод)
+  Serial.begin(9600); // Назначение скорости порта
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println(flag);
-  if(Serial.available() > 0)
+  if(Serial.available() > 0) //Проверка, что буфер передачи досутпен
   {
-    datafromUser=Serial.read();
+    datafromUser=Serial.read(); //Чтение буфера
   }
-       
-  if(datafromUser == '1')
+ // Начало. Определение и выполнение задания 
+  if(datafromUser == '1') 
   {
-    digitalWrite( LED_BUILTIN , HIGH );
-    status_1='1';
+    digitalWrite( LED_BUILTIN , HIGH ); //Включение диода
+    status_1='1'; //Назначение флага включенного диода
   }
-  else if(datafromUser == '0')
+  else if(datafromUser == '0') 
   {
-    digitalWrite( LED_BUILTIN, LOW);
-    status_1='0';
+    digitalWrite( LED_BUILTIN, LOW); //Выключение диода
+    status_1='0'; //Назначение флага выключенного диода
   }
-    else if(datafromUser == '2')
+  else if(datafromUser == '2') //Включение реле 1
   {
-    status_2='1';
+    status_2='1'; //Назаначение флага включенного реле 1
   }
-     else if(datafromUser == '3')
+  else if(datafromUser == '3') //Выключение реле 1
   {
-    status_2='0';
+    status_2='0'; // Назначение флага выключенного реле 1
   }
-    else if(datafromUser == '9')
+  else if(datafromUser == '9') //Получение команды отправки сатистики
   {
+ //Сборка массива для отправки статистики
     status_list[0]=status_1;
     status_list[1]=status_2;
     status_list[2]=status_3;
     status_list[3]=status_4;
-    Serial.println(status_list);
-    delay(1000);
+ //Конец сборки массива для отправки статистики
+    Serial.println(status_list); //Отправка массива статистики в порт
+    delay(1000);                 //Ожидание (нужно для корректной отправки, иначе данные дублируются и массив отдается не корректно)
   }
 
 }
